e1b536e28c7ddcc4c7eae3f882edcb614c7fb0dd
boldrini <luizboldriniej@gmail.com>
2024-08-06 01:19:49 -0400

    feat: Criado funções para buscar e armazenar dados

--- src/app/data/contexts/axios.ts
+++ src/app/data/contexts/axios.ts
@@ -3,4 +3,3 @@ import axios from "axios";
 export const axiosCliente = axios.create({
     baseURL: process.env.NEXT_PUBLIC_URL_BASE
 })
-
---
+++ src/app/data/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+'use client'
+import { useCallback } from 'react'
+
+export default function useLocalStorage() {
+    const get = useCallback((chave: string) => {
+        if(typeof window !=='undefined') {
+            const valorLocal = window?.localStorage?.getItem(chave)
+            return valorLocal ? JSON.parse(valorLocal) : null
+        }
+    }, [])
+
+    const set = useCallback((chave: string, valor: any) => {
+        window?.localStorage?.setItem(chave, JSON.stringify(valor))
+    }, [])
+
+    const remove = useCallback((chave: string) => {
+        const valorLocal = window?.localStorage?.removeItem(chave)
+    }, [])
+
+    return { get, set, remove }
+}
