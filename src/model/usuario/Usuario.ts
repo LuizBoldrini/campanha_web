28c5de16391e376c62a27a60c6ebdf8dc877e36b
boldrini <luizboldriniej@gmail.com>
2024-08-06 01:22:48 -0400

    feat: Novo model de usuários

---
+++ src/model/usuario/Usuario.ts
@@ -0,0 +1,11 @@
+export default interface Usuario {
+    codigoassociado?: string
+    nome?: string
+    matricula?: string
+    cpf: string
+    email?: string
+    telefone?: string
+    whatsapp?: string
+    nascimento: string
+    detalhe?: string
+}
