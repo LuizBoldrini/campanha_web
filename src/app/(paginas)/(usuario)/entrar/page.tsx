35812852f4000c53fa403af5a92ef4a43a713afc
boldrini <luizboldriniej@gmail.com>
2024-08-07 20:37:37 -0400

    feat: Criado rota de login

---
+++ src/app/(paginas)/(usuario)/entrar/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Suspense } from 'react'
+import FormUsuario from '@/components/usuario/FormUsuario'
+import Carregando from '@/components/shared/Carregando'
+
+export default function Page() {
+    return (
+        <Suspense fallback={<Carregando/>}>
+            <FormUsuario />
+        </Suspense>
+    )
+}
