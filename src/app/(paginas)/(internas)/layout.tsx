6027e5ec0a2f74f6930c7ff558093836aeb3bb1c
boldrini <luizboldriniej@gmail.com>
2024-08-07 20:35:07 -0400

    feat: Criado verificação de log para páginas internas

---
+++ src/app/(paginas)/(internas)/layout.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import MenuSuperior from '@/components/shared/MenuSuperior';
+import Pagina from '@/components/shared/Pagina';
+import { useAuth } from '@/app/data/contexts/AuthContext';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Carregando from '@/components/shared/Carregando';
+
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth();
+  const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted && !user) {
+      router.replace('/entrar');
+    }
+  }, [isMounted, user, router]);
+
+  if (!isMounted || !user) {
+    return (
+        <Carregando />
+    )
+  }
+
+  return (
+    <div>
+      <MenuSuperior />
+      <Pagina>{children}</Pagina>
+    </div>
+  );
+}
