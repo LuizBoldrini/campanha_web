cf95c8d80efe1114a22240477e071337e7380e27
boldrini <luizboldriniej@gmail.com>
2024-08-15 20:19:02 -0400

    feat: Criado page de comprovante

---
+++ src/app/(paginas)/(internas)/votar/sucesso/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import SucessoPage from "@/components/comprovante/SucessoPage"
+
+export default function PaginaSuscesso() {
+    return (
+        <div>
+            <SucessoPage />
+        </div>
+    )
+}
