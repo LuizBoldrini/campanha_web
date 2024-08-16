c88a934adcec747fdde47e8f6d8bd13e64884c60
boldrini <luizboldriniej@gmail.com>
2024-08-15 21:00:46 -0400

    fix: Correção do bg do menu superior

--- src/app/(paginas)/(inicial)/campanha/page.tsx
+++ src/app/(paginas)/(inicial)/campanha/page.tsx
@@ -1,19 +1,20 @@
-import NossasCampanhas from "@/components/campanha/NossasCampanhas";
-import MenuSuperior from "@/components/shared/MenuSuperior";
+import NossasCampanhas from "@/components/campanha/NossasCampanhas"
+import MenuSuperior from "@/components/shared/MenuSuperior"
 
 export default function PaginaCampanha() {
     return (
-        <div className="flex flex-col bg-zinc-900">
-           <MenuSuperior />
-        
-            <div
-                className="
-                    container flex flex-col lg:flex-row 
-                    items-center lg:items-start lg:justify-around 
-                    gap-10 lg:gap-0 py-10
-                "
-            >
-                <NossasCampanhas />
+        <div>
+            <MenuSuperior />
+            <div className="flex flex-col bg-zinc-900">
+                <div
+                    className="
+                        container flex flex-col lg:flex-row 
+                        items-center lg:items-start lg:justify-around 
+                        gap-10 lg:gap-0 py-10 
+                    "
+                >
+                    <NossasCampanhas />
+                </div>
             </div>
         </div>
     )
