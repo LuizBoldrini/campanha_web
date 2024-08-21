7ba11743b130ff5d8be03e391b9f4fc0aa610659
boldrini <luizboldriniej@gmail.com>
2024-08-21 17:34:44 -0400

    fix: Implementado bg.

--- src/components/usuario/FormUsuario.tsx
+++ src/components/usuario/FormUsuario.tsx
@@ -39,50 +39,54 @@ export default function FormUsuario() {
     <div className="flex justify-center items-center h-screen relative">
       <div
         className="
-          flex flex-col justify-center items-center gap-10
+          flex flex-col justify-center items-center gap-10 
           absolute top-0 left-0 w-full h-full
           bg-black/80 md:bg-transparent md:bg-gradient-to-r from-black/30 via-black/90 to-black/30
         "
       >
         <Logo />
-        <div className="flex flex-col w-1/2 gap-5 max-w-80">
-          <div className="flex flex-col gap-4 rounded min-w-[188px]">
-            <span>CPF</span>
-            <input
-              type="text"
-              value={CpfUtils.formatar(cpf)}
-              onChange={(e) => setCpf(CpfUtils.desformatar(e.target.value))}
-              placeholder="CPF"
-              ref={cpfRef}
-              onKeyDown={(e) => handleKey(e, nascimentoRef)}
-              className="bg-zinc-900 px-4 py-2 rounded"
-            />
-            <span>Nascimento</span>
-            <input
-              type="text"
-              value={NascimentoUtils.formatar(nascimento.toString())}
-              onChange={(e) => setNascimento(e.target.value)}
-              placeholder="Data de Nascimento"
-              ref={nascimentoRef}
-              onKeyDown={(e) => handleKey(e, entrarRef)}
-              className="bg-zinc-900 px-4 py-2 rounded"
-            />
-            {errorMessage && (
-              <span className='text-red-500 text-xs'>{errorMessage}</span>
-            )}
-            <div className="flex gap-5">
-              <button
-                onClick={handleLogin}
-                className="button bg-green-600 flex-1 hover:bg-green-700 active:bg-green-800"
-              >
-                Entrar
-              </button>
-              <button
-                onClick={() => router.push('/')}
-                className="button flex-1 hover:bg-zinc-700 active:bg-zinc-800"
-              >
-                Cancelar
-              </button>
+        <div className="flex flex-col w-full max-w-lg bg-zinc-800 p-10 rounded border border-zinc-700 shadow-lg">
+          <div className="flex flex-col">
+            <div className="flex flex-col gap-4 rounded min-w-[188px]">
+              <span>CPF</span>
+              <input
+                type="text"
+                value={CpfUtils.formatar(cpf)}
+                onChange={(e) => setCpf(CpfUtils.desformatar(e.target.value))}
+                placeholder="CPF"
+                ref={cpfRef}
+                maxLength={14}
+                onKeyDown={(e) => handleKey(e, nascimentoRef)}
+                className="bg-zinc-900 px-4 py-2 rounded"
+              />
+              <span>Nascimento</span>
+              <input
+                type="text"
+                value={NascimentoUtils.formatar(nascimento.toString())}
+                onChange={(e) => setNascimento(e.target.value)}
+                placeholder="Data de Nascimento"
+                ref={nascimentoRef}
+                maxLength={10}
+                onKeyDown={(e) => handleKey(e, entrarRef)}
+                className="bg-zinc-900 px-4 py-2 rounded"
+              />
+              {errorMessage && (
+                <span className='text-red-500 text-xs'>{errorMessage}</span>
+              )}
+              <div className="flex gap-5">
+                <button
+                  onClick={handleLogin}
+                  className="button bg-green-600 flex-1 hover:bg-green-700 active:bg-green-800"
+                >
+                  Entrar
+                </button>
+                <button
+                  onClick={() => router.push('/')}
+                  className="button flex-1 hover:bg-zinc-700 active:bg-zinc-800"
+                >
+                  Cancelar
+                </button>
+              </div>
             </div>
           </div>
         </div>
