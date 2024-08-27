f26bef30b9af61af62ca1cc8774634fb3f8811a0
boldrini <luizboldriniej@gmail.com>
2024-08-26 21:00:13 -0400

    fix: Removido icones não usados e implementado link para whatsapp

--- src/components/shared/Rodape.tsx
+++ src/components/shared/Rodape.tsx
@@ -1,16 +1,14 @@
 import {
     IconBrandFacebook,
     IconBrandInstagram,
-    IconBrandLinkedin,
     IconBrandWhatsapp,
-    IconBrandYoutube,
 } from '@tabler/icons-react'
 import Link from 'next/link'
 
 export default function Rodape() {
     return (
         <footer className="flex items-center bg-black ">
-            <div className="container flex flex-col gap-2 py-2">
+            <div className="container flex flex-col gap-4 py-4">
                 <div className="flex flex-col md:flex-row items-center md:items-start md:justify-between gap-5">
                     <div className="flex flex-col gap-1 items-center md:items-start">
                         <span className="text-2xl text-zinc-300 font-bold mb-2.5">Sobre</span>
@@ -21,10 +19,13 @@ export default function Rodape() {
                     <div className="flex flex-col gap-1 items-center md:items-start">
                         <span className="text-2xl text-zinc-300 font-bold mb-2.5">Contato</span>
                         <span className="text-sm text-zinc-400">asmuv_vha@hotmail.com</span>
-                        <div className="flex items-center gap-2 text-sm text-zinc-400">
-                            <IconBrandWhatsapp size={20} className="text-green-500" />
-                            <span>69 3322-2335</span>
-                        </div>
+                        <Link href={`https://wa.me/556933222335`} target="_blank">
+                            <div className="flex items-center gap-1 text-sm text-zinc-400">
+                                <IconBrandWhatsapp size={24} className="text-green-500" />
+                                <span>69 3322-2335</span>
+                            </div>
+                        </Link>
+                            
                     </div>
                 </div>
                 <div className="flex flex-col md:flex-row justify-between items-center gap-5">
@@ -39,7 +40,12 @@ export default function Rodape() {
                     <div className="flex flex-col md:flex-row items-center gap-1.5 text-zinc-400 text-sm">
                         <div className="flex gap-1.5">
                             <span>Desenvolvido por </span>
-                            <span><strong>EasyVote Systems</strong></span>
+                            <Link href={`https://wa.me/556993414639`} target="_blank">
+                            <div className="flex items-center gap-1">
+                                <IconBrandWhatsapp size={18} className="text-green-500" />
+                                <span><strong>EasyVote Systems</strong></span>
+                            </div>
+                            </Link>    
                         </div>
                         <span className="hidden md:inline-block">-</span>
                         <span>Todos os direitos reservados</span>
