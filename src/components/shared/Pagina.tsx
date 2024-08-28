7f863100aa00243d5cc21f40ec18eb7f40de020a
boldrini <luizboldriniej@gmail.com>
2024-08-27 22:41:21 -0400

    feat: Implementado na interface sedeve ou não mostrar o rodapé

--- src/components/shared/Pagina.tsx
+++ src/components/shared/Pagina.tsx
@@ -3,15 +3,18 @@ import Rodape from "./Rodape"
 export interface PaginaProps {
   children: React.ReactNode
   bgClass?: string
+  mostraRodape?: boolean
 }
+
 export default function Pagina({
   children,
   bgClass = "bg-zinc-900",
+  mostraRodape = true,
 }: PaginaProps) {
   return (
-    <div className={`flex flex-col min-h-screen w-screen ${bgClass}`}>
+    <div className={`flex flex-col min-h-screen ${bgClass}`}>
       <main className="flex-grow">{children}</main>
-      <Rodape />
+      {mostraRodape && <Rodape />}
     </div>
   )
 }
