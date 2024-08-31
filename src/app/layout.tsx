64a9202e92b7b226b69a64e62785558617788a03
boldrini <luizboldriniej@gmail.com>
2024-08-31 12:09:29 -0400

    fix: alterado caminho do favincon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Votação Online",
   description: "Realize sua votação de forma rápida e segura",
-  icons: "../public/favicon.ico",
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
@@ -19,12 +21,6 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className="dark">
       <body className={`${inter.className} overflow-x-hidden`}>
-        <link
-          rel="icon"
-          href="/favicon.ico"
-          sizes="any"
-          className="rounded-full"
-        />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
