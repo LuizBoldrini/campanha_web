bfa48845443b47871606920aa8ea6b63b6ee7c48
boldrini <luizboldriniej@gmail.com>
2024-09-15 15:43:44 -0400

    fix: Implementado bloqueiaVotar para o login

--- src/app/(paginas)/(usuario)/entrar/page.tsx
+++ src/app/(paginas)/(usuario)/entrar/page.tsx
@@ -2,11 +2,13 @@
 import { Suspense } from "react"
 import FormUsuario from "@/components/usuario/FormUsuario"
 import Carregando from "@/components/shared/Carregando"
+import BloquearVoltar from "@/components/shared/BloquearVoltar"
 
 export default function Page() {
   return (
     <Suspense fallback={<Carregando />}>
       <FormUsuario />
+      <BloquearVoltar />
     </Suspense>
   )
 }
